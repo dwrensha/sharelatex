f90cc9c1dfce5c83e4678b9044fea4166a28e1a6
David Renshaw <david@sandstorm.io>
2017-01-16 18:56:04 -0500

    getting starting on moving to vagrant-spk

---
+++ .sandstorm/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
+set -euo pipefail
+# This is the ideal place to do things like:
+#
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y nginx nodejs nodejs-legacy
+apt-get install -y texlive
+
+exit 0
